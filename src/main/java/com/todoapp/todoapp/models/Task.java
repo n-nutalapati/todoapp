196b8b9e6ebe05542a91ca26b2b152c2cf2487dd
n-nutalapati <naveenkumar.nutalapati@outlook.com>
2024-12-27 20:15:49 +0530

    Initial Commit

---
+++ src/main/java/com/todoapp/todoapp/models/Task.java
@@ -0,0 +1,51 @@
+package com.todoapp.todoapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private boolean completed;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", completed=" + completed +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/todoapp/todoapp/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.todoapp.todoapp.repository;
+
+import com.todoapp.todoapp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
